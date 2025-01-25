666d30dc8b6cfff20c48cde0feb2a102dc2c1926
Mehedi Hasan Bappi <mehedibubt34@gmail.com>
2025-01-25 16:34:32 +0600

    upgrade to tailwind v4

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -2,7 +2,7 @@ import { Link } from '@tanstack/react-router'
 
 const Logo = () => (
   <Link to='/' className='font-extrabold text-3xl'>
-    <span className='bg-gradient-to-r from-blue-500 to-rose-500 text-transparent bg-clip-text'>
+    <span className='bg-linear-to-r from-blue-500 to-rose-500 text-transparent bg-clip-text'>
       Linkly
     </span>
   </Link>
